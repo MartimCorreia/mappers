b9c2c922044cb80c46b5309c4a387740a880dad1
Martim Correia <correia_martim@hotmail.com>
2022-10-25 11:08:03 +0100

    can pin the map on click and get user location

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -10,9 +10,32 @@ export default class extends Controller {
     container: this.element,
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    zoom: 1 // starting zoom
 
     });
-  
+
+    this.map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+      })
+      );
+
+      this.map.on('click', (e) => {
+        const marker = new mapboxgl.Marker({
+          color: "#F76E11",
+          draggable: true
+        }).setLngLat([JSON.stringify(e.lngLat.wrap().lng), JSON.stringify(e.lngLat.wrap().lat)]).addTo(this.map);
+        // console.log(JSON.stringify(e.lngLat.wrap().lng));
+        console.log()
+      });
+
+
+
   }
 }
